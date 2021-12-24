eae97ac255071c34d537ddb71d82205785babe76
3140102640@zju.edu.cn <31401402640@zju.edu.cn>
2021-12-24 20:06:00 +0800

    implement r tree

---
+++ src/r_tree.py
@@ -0,0 +1,46 @@
+import os
+import random
+
+from rtree import index
+
+from src.common_utils import Point
+
+
+class RTree:
+    def __init__(self):
+        p = index.Property()
+        self.index = index.Index(properties=p)
+
+    def insert(self, point):
+        self.index.insert(point.index, (point.lng, point.lat))
+
+    def search(self, point):
+        search_result = self.index.intersection((point.lng, point.lat))
+        search_result_list = list(search_result)
+        return search_result_list[0] if len(search_result_list) != 0 else -1
+
+    def delete(self, point):
+        return self.index.delete(point.index, (point.lng, point.lat))
+
+
+def create_data_and_search():
+    r_tree = RTree()
+    lat, lng, index = 0, 0, 0
+    for i in range(101):
+        lng = random.uniform(-180, 180)
+        lat = random.uniform(-90, 90)
+        index += 1
+        r_tree.insert(Point(lng, lat, index))
+    test_point = Point(lng, lat)
+    search_index = r_tree.search(test_point)
+    print("{0} is found in {1}".format(test_point, search_index))
+    delete_index = r_tree.delete(test_point)
+    print("{0} is deleted in {1}".format(test_point, delete_index))
+
+
+if __name__ == '__main__':
+    os.chdir('D:\\Code\\Paper\\st-learned-index')
+    path = 'data/test_x_y_index.csv'
+    # create_data(path)
+    create_data_and_search()
+    # read_data_and_search(path)
