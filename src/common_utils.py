5bc9ddd511519748874e8ccef3aca2463a6f9be3
3140102640@zju.edu.cn <31401402640@zju.edu.cn>
2021-12-24 20:05:40 +0800

    move point into common utils

---
+++ src/common_utils.py
@@ -0,0 +1,25 @@
+class Point:
+    def __init__(self, lng, lat, index=0):
+        self.lng = lng
+        self.lat = lat
+        self.index = index
+
+    def __eq__(self, other):
+        if other.lng == self.lng and other.lat == self.lat:
+            return True
+        else:
+            return False
+
+    def __str__(self):
+        return "Point({0}, {1}, {2})".format(self.lng, self.lat, self.index)
+
+    def near(self, other):
+        """
+        近似相等，只要整数部分一致即可
+        :param other:
+        :return:
+        """
+        if int(other.lng) == int(self.lng) and int(other.lat) == int(self.lat):
+            return True
+        else:
+            return False
