2346d8488ce1a4a0db459b1d6a9fafef6da56339
3140102640@zju.edu.cn <31401402640@zju.edu.cn>
2021-12-31 15:31:04 +0800

    implement zm-index

--- src/common_utils.py
+++ src/common_utils.py
@@ -2,13 +2,15 @@ import csv
 import random
 import time
 
+import morton
 import pandas as pd
 
 
 class Point:
-    def __init__(self, lng, lat, index=0):
+    def __init__(self, lng, lat, z=None, index=None):
         self.lng = lng
         self.lat = lat
+        self.z = z
         self.index = index
 
     def __eq__(self, other):
@@ -31,6 +33,9 @@ class Point:
         else:
             return False
 
+    def compute_z(self):
+        self.z = ZOrder().point_to_z(self.lng, self.lat)
+
 
 class Region:
     def __init__(self, bottom, up, left, right):
@@ -41,6 +46,27 @@ class Region:
 
     def contain(self, point):
         return self.up >= point.lat >= self.bottom and self.right >= point.lng >= self.left
+
+
+class ZOrder:
+    def __init__(self):
+        self.morton = morton.Morton(dimensions=2, bits=21)
+
+    def point_to_z(self, lng, lat):
+        """
+        计算point的z order
+        1. degree的point坐标映射到整数，以便计算z order
+        zoom的大小取决于point的精度，当前数据的范围是Range(40, 42, -75, -73)，经纬度有6位小数
+        则zoom差不多是7位
+        2. 使用morton-py.pack(int, int): int计算z order
+        :param lng:
+        :param lat:
+        :return:
+        """
+        zoom = 1000000
+        lng_zoom = int((lng - -75.0) * zoom)
+        lat_zoom = int((lat - 40.0) * zoom)
+        return self.morton.pack(lng_zoom, lat_zoom)
 
 
 def create_data(path):
@@ -54,14 +80,42 @@ def create_data(path):
             writer.writerow([index, lng, lat])
 
 
-def read_data_and_search(path, index):
+def create_data_z(input_path, output_path, lng_col, lat_col):
+    """
+    compute and add z order into file
+    1. compute z order by lng and lat
+    2. add z order into file
+    :param input_path:
+    :param output_path:
+    :param lng_col:
+    :param lat_col:
+    :return:
+    """
+    df = pd.read_csv(input_path, header=None)
+    z_order = ZOrder()
+    z_values = []
+    for i in range(df.count()[0]):
+        z_values.append(z_order.point_to_z(df[lng_col][i], df[lat_col][i]))
+    df["z_value"] = z_values
+    df = df.rename(columns={lng_col: "lng", lat_col: "lat"})
+    df = df.sort_values(['z_value'], ascending=[True])
+    df = df.reset_index()
+    df = df.drop(columns=["index"])
+    df.to_csv(output_path, index_label="index", header=None)
+
+
+def read_data_and_search(path, index, lng_col, lat_col, z_col, index_col):
     index_name = index.name
-    data = pd.read_csv(path, header=None)
+    data = pd.read_csv(path)
     train_set_point = []
     test_set_point = []
     test_ratio = 0.5  # 测试集占总数据集的比例
-    for i in range(int(data.shape[0])):
-        train_set_point.append(Point(data.iloc[i, 1], data.iloc[i, 2], data.iloc[i, 0]))
+    if lng_col and lat_col:
+        for i in range(int(data.shape[0])):
+            train_set_point.append(Point(data.iloc[i, lng_col], data.iloc[i, lat_col], None, data.iloc[i, index_col]))
+    elif z_col:
+        for i in range(int(data.shape[0])):
+            train_set_point.append(Point(None, None, data.iloc[i, z_col], data.iloc[i, index_col]))
     test_set_point = train_set_point[:int(len(train_set_point) * test_ratio)]
 
     print("*************start %s************" % index_name)
---
+++ src/spatial_index/zm_index.py
@@ -0,0 +1,99 @@
+import gc
+import os
+
+from src.b_tree import BTree
+from src.common_utils import read_data_and_search
+from src.index import Index
+from src.rmi import TrainedNN, AbstractNN
+
+
+class ZMIndex(Index):
+    def __init__(self):
+        super(ZMIndex, self).__init__("ZM Index")
+        self.block_size = 100
+        self.total_number = 200000
+        self.use_thresholds = [True, False]
+        self.thresholds = [1, 4]
+        self.stages = [1, 10]
+        self.cores = [[1, 1], [1, 1]]
+        self.train_steps = [20000, 20000]
+        self.batch_sizes = [50, 50]
+        self.learning_rates = [0.0001, 0.0001]
+        self.keep_ratios = [1.0, 1.0]
+        self.index = None
+
+    def build(self, points):
+        stage_length = len(self.stages)
+        train_inputs = [[[] for i in range(self.stages[i])] for i in range(stage_length)]
+        train_labels = [[[] for i in range(self.stages[i])] for i in range(stage_length)]
+        index = [[None for i in range(self.stages[i])] for i in range(stage_length)]
+        train_inputs[0][0] = [point.z for point in points]
+        train_labels[0][0] = [point.index for point in points]
+        # 构建stage_nums结构的树状NNs
+        for i in range(0, stage_length):
+            for j in range(0, self.stages[i]):
+                if len(train_labels[i][j]) == 0:
+                    continue
+                inputs = train_inputs[i][j]
+                labels = []
+                # 非叶子结点决定下一层要用的NN是哪个
+                if i < stage_length - 1:
+                    # first stage, calculate how many models in next stage
+                    divisor = self.stages[i + 1] * 1.0 / (self.total_number / self.block_size)
+                    for k in train_labels[i][j]:
+                        labels.append(int(k * divisor))
+                else:
+                    labels = train_labels[i][j]
+                # train model
+                tmp_index = TrainedNN(inputs, labels,
+                                      self.thresholds[i],
+                                      self.use_thresholds[i],
+                                      self.cores[i],
+                                      self.train_steps[i],
+                                      self.batch_sizes[i],
+                                      self.learning_rates[i],
+                                      self.keep_ratios[i])
+                tmp_index.train()
+                # get parameters in model (weight matrix and bias matrix)
+                index[i][j] = AbstractNN(tmp_index.get_weights(),
+                                         tmp_index.get_bias(),
+                                         self.cores[i],
+                                         tmp_index.cal_err())
+                del tmp_index
+                gc.collect()
+                if i < stage_length - 1:
+                    # allocate data into training set for models in next stage
+                    for ind in range(len(train_inputs[i][j])):
+                        # pick model in next stage with output of this model
+                        p = index[i][j].predict(train_inputs[i][j][ind])
+                        if p > self.stages[i + 1] - 1:
+                            p = self.stages[i + 1] - 1
+                        train_inputs[i + 1][p].append(train_inputs[i][j][ind])
+                        train_labels[i + 1][p].append(train_labels[i][j][ind])
+
+        # 如果叶节点NN的精度低于threshold，则使用Btree来代替
+        for i in range(self.stages[stage_length - 1]):
+            if index[stage_length - 1][i] is None:
+                continue
+            mean_abs_err = index[stage_length - 1][i].mean_err
+            if mean_abs_err > self.thresholds[stage_length - 1]:
+                # replace model with BTree if mean error > threshold
+                print("Using BTree")
+                index[stage_length - 1][i] = BTree(2)
+                index[stage_length - 1][i].build(train_inputs[stage_length - 1][i], train_labels[stage_length - 1][i])
+        self.index = index
+
+    def predict(self, point):
+        stage_length = len(self.stages)
+        pre = 0
+        for i in range(0, stage_length):
+            pre = self.index[i][pre].predict(point.z)
+        return pre
+
+
+if __name__ == '__main__':
+    os.chdir('/')
+    path = 'data/test_x_y_index.csv'
+    index = ZMIndex()
+    read_data_and_search(path, index)
+    # TODO: z order logic
