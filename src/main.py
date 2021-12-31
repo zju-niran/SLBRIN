c0ac59bb442a6b95777a67e7352162e29b41445a
3140102640@zju.edu.cn <31401402640@zju.edu.cn>
2021-12-31 15:32:14 +0800

    create the main to test the index

---
+++ src/main.py
@@ -0,0 +1,18 @@
+import os
+
+from src.common_utils import read_data_and_search, create_data_z
+from src.spatial_index.quad_tree import QuadTree
+from src.spatial_index.r_tree import RTree
+from src.spatial_index.zm_index import ZMIndex
+
+if __name__ == '__main__':
+    os.chdir('D:\\Code\\Paper\\st-learned-index')
+    path = 'C:\\Users\\123\\Desktop\\trip_data_2.csv'
+    path_100000 = 'C:\\Users\\123\\Desktop\\trip_data_2_100000.csv'
+    path_100000_z = 'C:\\Users\\123\\Desktop\\trip_data_2_100000_z.csv'
+    # create_data_z(path_100000, path_100000_z, 1, 2)
+    # path_test = 'data/test_x_y_index.csv'
+    # create_data(path_test)
+    # read_data_and_search(path_100000, RTree(), 1, 2, None, 0)
+    # read_data_and_search(path_100000, QuadTree(), 1, 2, None, 0)
+    read_data_and_search(path_100000_z, ZMIndex(), None, None, 5, 0)
