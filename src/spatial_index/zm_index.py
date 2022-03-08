472b879cfd21ecdccbe0c96caefdee7278d8ba3d
Niran814804102 <3140102640@zju.edu.cn>
2022-03-08 22:29:19 +0800

    实现zm_index的range_query

--- src/spatial_index/zm_index.py
+++ src/spatial_index/zm_index.py
@@ -9,15 +9,15 @@ import numpy as np
 import pandas as pd
 
 sys.path.append('/home/zju/wlj/st-learned-index')
-from src.spatial_index.common_utils import ZOrder, Region, Point
+from src.spatial_index.common_utils import ZOrder, Region, binary_search, Point
 from src.spatial_index.spatial_index import SpatialIndex
 from src.rmi_keras import TrainedNN, AbstractNN
 
 
 class ZMIndex(SpatialIndex):
     def __init__(self, region=Region(-90, 90, -180, 180), model_path=None, train_data_length=None, rmi=None,
-                 index_list=None, block_size=None, use_thresholds=None, thresholds=None, stages=None, cores=None,
-                 train_steps=None, batch_sizes=None, learning_rates=None, retrain_time_limits=None,
+                 index_list=None, point_list=None, block_size=None, use_thresholds=None, thresholds=None, stages=None,
+                 cores=None, train_steps=None, batch_sizes=None, learning_rates=None, retrain_time_limits=None,
                  thread_pool_size=None):
         super(ZMIndex, self).__init__("ZM Index")
         # nn args
@@ -40,7 +40,8 @@ class ZMIndex(SpatialIndex):
         self.model_path = model_path
         self.train_data_length = train_data_length
         self.rmi = [[None for i in range(self.stages[i])] for i in range(self.stage_length)] if rmi is None else rmi
-        self.index_list = index_list  # 索引列
+        self.index_list = index_list
+        self.point_list = point_list
 
     def init_train_data(self, data: pd.DataFrame):
         """
@@ -53,10 +54,14 @@ class ZMIndex(SpatialIndex):
         :return: None
         """
         z_order = ZOrder(dimensions=2, bits=21, region=self.region)
-        z_values = data.apply(lambda t: z_order.point_to_z(t.x, t.y), 1)
-        self.train_data_length = len(z_values)
-        self.train_inputs[0][0] = z_values.sort_values(ascending=True).values
+        data["z"] = data.apply(lambda t: z_order.point_to_z(t.x, t.y), 1)
+        data.sort_values(by=["z"], ascending=True, inplace=True)
+        data.reset_index(drop=True, inplace=True)
+        self.train_data_length = len(data)
+        self.train_inputs[0][0] = data.z.values
         self.train_labels[0][0] = pd.Series(np.arange(0, self.train_data_length) / self.block_size).values
+        self.index_list = data.z.values.tolist()
+        self.point_list = data[["x", "y"]].values.tolist()
 
     def build_single_thread(self, curr_stage, current_stage_step, inputs, labels, tmp_dict=None):
         # train model
@@ -137,7 +142,6 @@ class ZMIndex(SpatialIndex):
             self.rmi[i][key] = value
 
         # 3. clear train data and label to save memory
-        self.index_list = self.train_inputs[0][0]
         self.train_inputs = None
         self.train_labels = None
 
@@ -153,8 +157,6 @@ class ZMIndex(SpatialIndex):
         leaf_model_index = 0
         for i in range(0, self.stage_length - 1):
             leaf_model_index = round(self.rmi[i][leaf_model_index].predict(key))
-            if leaf_model_index > self.stages[i + 1] - 1:
-                leaf_model_index = self.stages[i + 1] - 1
         # 2. predict the index by leaf_model
         leaf_model = self.rmi[self.stage_length - 1][leaf_model_index]
         pre = leaf_model.predict(key)
@@ -167,7 +169,8 @@ class ZMIndex(SpatialIndex):
         """
         if os.path.exists(self.model_path) is False:
             os.makedirs(self.model_path)
-        np.savetxt(self.model_path + 'index_list.csv', self.index_list, delimiter=',', fmt='%.f')
+        np.savetxt(self.model_path + 'index_list.csv', self.index_list, delimiter=',', fmt='%d')
+        np.savetxt(self.model_path + 'point_list.csv', self.point_list, delimiter=',', fmt='%f,%f')
         with open(self.model_path + 'zm_index.json', "w") as f:
             json.dump(self, f, cls=MyEncoder, ensure_ascii=False)
 
@@ -180,7 +183,8 @@ class ZMIndex(SpatialIndex):
             zm_index = json.load(f, cls=MyDecoder)
             self.train_data_length = zm_index.train_data_length
             self.rmi = zm_index.rmi
-            self.index_list = np.loadtxt(self.model_path + 'index_list.csv', delimiter=",").tolist()
+            self.index_list = np.loadtxt(self.model_path + 'index_list.csv', dtype=np.int64, delimiter=",").tolist()
+            self.point_list = np.loadtxt(self.model_path + 'point_list.csv', dtype=np.float, delimiter=",").tolist()
             del zm_index
 
     @staticmethod
@@ -188,9 +192,10 @@ class ZMIndex(SpatialIndex):
         return ZMIndex(region=d['region'],
                        train_data_length=d['train_data_length'],
                        rmi=d['rmi'],
-                       index_list=d['index_list'])
+                       index_list=d['index_list'],
+                       point_list=d['point_list'])
 
-    def point_query(self, points, z=None):
+    def point_query(self, points):
         """
         query index by x/y point
         1. compute z from x/y of points
@@ -199,7 +204,7 @@ class ZMIndex(SpatialIndex):
         :param points: list, [x, y]
         :return: list, [pre]
         """
-        z_order = ZOrder(dimensions=2, bits=21, region=self.region) if z is None else z
+        z_order = ZOrder(dimensions=2, bits=21, region=self.region)
         results = []
         for point in points:
             # 1. compute z from x/y of points
@@ -209,50 +214,53 @@ class ZMIndex(SpatialIndex):
             left_bound = max((pre - max_err) * self.block_size, 0)
             right_bound = min((pre - min_err) * self.block_size, self.train_data_length - 1)
             # 3. binary search in scope
-            result = self.binary_search(self.index_list, z_value, round(left_bound), round(right_bound))
+            result = binary_search(self.index_list, z_value, round(left_bound), round(right_bound))
             if result is not None:
                 result /= self.block_size
             results.append(result)
         return results
 
-    # def range_query(self, data: pd.DataFrame):
-    #     """
-    #     query index by x1/y1/x2/y2 range
-    #     1. compute z1, z2 from x1/y1/x2/y2 of data
-    #     2. normalize z1 and z2 by z.min and z.max
-    #     3. point query from z1 and z2 to index_z1 and index_z2
-    #     4. filter all the point of scope[index_z1, index_z2] by range(x1/y1/x2/y2).contain(point)
-    #     :param data: pd.DataFrame, [x1, y1, x2, y2]
-    #     :return: pd.DataFrame, [pre, min_err, max_err]
-    #     """
-    #     z_order = ZOrder()
-    #     z_value = z_order.point_to_z(df[lng_col][i], df[lat_col][i])
-    #     # z归一化
-    #     min_z_value = min(z_values)
-    #     max_z_value = max(z_values)
-    #     for i in range(df.count()[0]):
-    #         z_value_normalization = (z_values[i] - min_z_value) / (max_z_value - min_z_value)
-    #         z_values_normalization.append(z_value_normalization)
-
-    # TODO: 无法处理有重复的数组
-    def binary_search(self, nums, x, left, right):
+    def range_query(self, windows):
         """
-        binary search x in nums[left, right]
-        :param nums: list, value list
-        :param x: value
-        :param left:
-        :param right:
-        :return: index
+        query index by x1/y1/x2/y2 window
+        1. compute z from window_left and window_right
+        2. find index_left by point query
+        3. find index_right by point query
+        4. filter all the points of scope[index_left, index_right] by range(x1/y1/x2/y2).contain(point)
+        :param windows: list, [x1, y1, x2, y2]
+        :return: list, [pres]
         """
-        while left <= right:
-            mid = (left + right) // 2
-            if nums[mid] == x:
-                return mid
-            if nums[mid] < x:
-                left = mid + 1
-            else:
-                right = mid - 1
-        return None
+        z_order = ZOrder(dimensions=2, bits=21, region=self.region)
+        results = []
+        for window in windows:
+            # 1. compute z of window_left and window_right
+            z_value1 = z_order.point_to_z(window[2], window[0])
+            z_value2 = z_order.point_to_z(window[3], window[1])
+            # 2. find index_left by point query
+            # if point not found, index_left = pre - min_err
+            pre1, min_err1, max_err1 = self.predict(z_value1)
+            left_bound1 = round(max((pre1 - max_err1) * self.block_size, 0))
+            right_bound1 = round(min((pre1 - min_err1) * self.block_size, self.train_data_length - 1))
+            index_left = binary_search(self.index_list, z_value1, left_bound1, right_bound1)
+            if index_left is None:
+                index_left = left_bound1
+            # 3. find index_right by point query
+            # if point not found, index_right = pre - max_err
+            pre2, min_err2, max_err2 = self.predict(z_value2)
+            left_bound2 = round(max((pre2 - max_err2) * self.block_size, 0))
+            right_bound2 = round(min((pre2 - min_err2) * self.block_size, self.train_data_length - 1))
+            index_right = binary_search(self.index_list, z_value2, left_bound2, right_bound2)
+            if index_right is None:
+                index_right = right_bound2
+            # 4. filter all the point of scope[index1, index2] by range(x1/y1/x2/y2).contain(point)
+            tmp_results = []
+            region = Region(window[0], window[1], window[2], window[3])
+            for index in range(index_left, index_right + 1):
+                point = self.point_list[index]
+                if region.contain_and_border(Point(point[0], point[1])):
+                    tmp_results.append(index)
+            results.append(tmp_results)
+        return results
 
 
 class MyEncoder(json.JSONEncoder):
@@ -325,11 +333,23 @@ if __name__ == '__main__':
         build_time = end_time - start_time
         print("Build %s time " % index_name, build_time)
         index.save()
-    train_set_xy_list = np.delete(train_set_xy.values, 0, 1).tolist()
+    print("*************start point query************")
+    point_query_list = train_set_xy.drop("index", axis=1).values.tolist()
     start_time = time.time()
-    result = index.point_query(train_set_xy_list)
+    results = index.point_query(point_query_list)
     end_time = time.time()
-    search_time = (end_time - start_time) / len(train_set_xy_list)
-    print("Search time ", search_time)
-    print("Not found nums ", pd.Series(result).isna().sum())
+    search_time = (end_time - start_time) / len(point_query_list)
+    print("Point query time ", search_time)
+    print("Not found nums ", pd.Series(results).isna().sum())
+    print("*************start range query************")
+    path = '../../data/trip_data_1_range_query.csv'
+    range_query_df = pd.read_csv(path, usecols=[1, 2, 3, 4, 5])
+    range_query_list = range_query_df.drop("count", axis=1).values.tolist()
+    start_time = time.time()
+    results = index.range_query(range_query_list)
+    end_time = time.time()
+    search_time = (end_time - start_time) / len(range_query_list)
+    print("Range query time ", search_time)
+    range_query_df["query"] = pd.Series(results).apply(len)
+    print("Not found nums ", (range_query_df["query"] != range_query_df["count"]).sum())
     print("*************end %s************" % index_name)
